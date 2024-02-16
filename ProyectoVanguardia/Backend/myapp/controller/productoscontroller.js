b35cc3a39d7a37a8d7c073b53448ccda8f490b60
Milton Rigoberto Arita <122846565+milton8arita@users.noreply.github.com>
2024-02-15 20:11:30 -0600

    Update productoscontroller.js
    actualice relación de talas nuevas

--- ProyectoVanguardia/Backend/myapp/controller/productoscontroller.js
+++ ProyectoVanguardia/Backend/myapp/controller/productoscontroller.js
@@ -1,7 +1,10 @@
 const Productos = require('../model/productosmodel');
 
-function visualizar (req, res){
-
+function visualizar(req, res){
+    Productos.find({}).then(Productos =>{
+        if(Productos.length)return res.render('index', {Productos})
+        return res.status('204').send({message: 'No Conten'})
+    }).catch(err => res.status(500).send({err}))
 }
 
 function  crear (req, res){
@@ -9,6 +12,7 @@ function  crear (req, res){
         id: req.body.id,
         nommbre: req.body.nommbre,
         descripcion: req.body.descripcion,
+        precio: req.body.precio,
         gasolinera: req.body.gasolinera
     })
     productos.save().then(
@@ -16,12 +20,21 @@ function  crear (req, res){
     ).catch(err =>res.status(500).send({err}))
 }
 
-function editar (req, res){
-
+function editar(req, res){
+    const id = req.body.id_editar
+    const nombre = req.body.nombre_editar
+    const descripcion = req.body.latitud_editar
+    const precio = req.body.precio_editar
+    Productos.findByIdAndUpdate(id, {nombre:nombre, descripcion:descripcion, precio:precio }).then(
+        res.redirect('/')
+    ).catch(err => res.status(500).send({err}))
 }
 
 function eliminar (req, res){
-
+    const id = req.params.id
+    Productos.findByIdAndDelete(id).then(
+        res.redirect('/')
+    ).catch(err = res.status(500).send({err}))
 }
 
 module.exports = {
